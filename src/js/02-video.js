c0bc0cdfac50992d53583b7ca5a2d2e8e3b40607
Ivan Kornienko <ivan@homeds.pro>
2023-03-13 23:28:34 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function setTimeToLocalStorage(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+player.on('timeupdate', throttle(setTimeToLocalStorage, 1000));
+
+const timeStart = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(timeStart)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
